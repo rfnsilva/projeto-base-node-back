b1e425de12d4acb73a444abe6bd6f5d67c254615
ricardo <rfnsbadoo@gmail.com>
2020-09-30 02:24:06 -0300

    commit final

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
-import "reflect-metadata";
-import {createConnection} from "typeorm";
+import 'reflect-metadata';
+import { createConnection } from 'typeorm';
 import express from 'express';
-import * as bodyParser from "body-parser";
-import routes from "./routes";
 import * as dotenv from 'dotenv';
+import * as bodyParser from 'body-parser';
+
+import routes from './routes';
 
 const app = express();
 
